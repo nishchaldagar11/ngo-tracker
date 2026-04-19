64f98cda786c9cfb814c151295422fa9514b815b
Nishchal Dagar <dagarnishchal@gmail.com>
2026-04-19 15:01:20 +0530

    final fix vercel build

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,9 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
+    host: true,
+    port: 5173
   },
   build: {
-    outDir: "dist"
+    outDir: "dist",
+    emptyOutDir: true
   }
 });
